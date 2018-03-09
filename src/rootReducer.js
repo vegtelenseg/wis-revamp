0a9d58bebdef606093bec51a0c2250db0a937ff8
Siyanda-Mzam <dot.netntoz@gmail.com>
2018-03-09 09:24:25 +0200

    First build for heroku

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import searchReducer from './components/search/search.reducer';
 import mapReducer from './components/map/map.reducer';
+import mapMarkerReducer from './components/mapMarker/mapMarker.reducer';
 
 export default combineReducers({
   searchReducer,
-  mapReducer
+	mapReducer,
+	mapMarkerReducer
 });
