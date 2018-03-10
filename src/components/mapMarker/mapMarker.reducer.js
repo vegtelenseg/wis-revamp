5fadaeaf4102cbcc3c434b50a4efe2d8df3f7bde
Siyanda-Mzam <dot.netntoz@gmail.com>
2018-03-10 15:58:37 +0200

    Added a balloon for item information on click

--- src/components/mapMarker/mapMarker.reducer.js
+++ src/components/mapMarker/mapMarker.reducer.js
@@ -1,24 +1,30 @@
 const initialState = {
-  product: {}
+  isVisible: false
 };
 
-const actionTypes = {
-  SET_ITEM_INFO: 'SET_ITEM_INFO'
+const mapMarkerActionTypes = {
+  SHOW_ITEM_INFO: 'SHOW_ITEM_INFO'
 };
 
 export const mapMarkerActions = {
   watchItem: function(item) {
     return;
+  },
+  showItemInfo: function(predicate) {
+    return {
+      type: mapMarkerActionTypes.SHOW_ITEM_INFO,
+      payload: predicate
+    };
   }
 };
 
 export default function mapMarkerReducer(state = initialState, action) {
   switch (action.type) {
-    case actionTypes.SET_ITEM_INFO:
+    case mapMarkerActionTypes.SHOW_ITEM_INFO:
       return {
-        ...state
+        ...state,
+        isVisible: !action.payload
       };
-
     default:
       return state;
   }
