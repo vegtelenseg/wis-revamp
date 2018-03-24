57fb832e62fc22542daaeae337e6088de8506671
Siya Mzam <dot.netntoz@gmail.com>
2018-03-24 18:25:25 +0200

    Added get current location of user functionality.

--- src/components/map/map.view.js
+++ src/components/map/map.view.js
@@ -6,22 +6,16 @@ import PropTypes from 'prop-types';
 import MapMarker from '../mapMarker/mapMarker.container';
 
 export default class MapView extends Component {
+
+	componentDidMount() {
+		this.props.getUserCurrentLocationThunk();
+	}
   render() {
-		const { currentUserLocation, center } = this.props;
-		const coords = null;
-		if ("geolocation" in navigator) {
-			navigator.geolocation.getCurrentPosition(position => {
-				const coords = [position.coords.latitude, position.coords.longitude];
-				console.log("The coords: ", coords);
-				return coords;
-			});
-		} else {
-			alert("Please switch on the location service of your device");
-		}
+		const { foundItems, currentUserLocation } = this.props;
     return (
-      <Map center={coords || currentUserLocation} zoom={12} position={coords || center}>
+      <Map center={currentUserLocation} zoom={12}>
         <TileLayer url={serviceUrl.tileLayerUrl} />
-        {<MapMarker foundItems={this.props.foundItems} />}
+        {<MapMarker foundItems={foundItems} />}
       </Map>
     );
   }
@@ -30,5 +24,6 @@ export default class MapView extends Component {
 Map.propTypes = {
   center: PropTypes.array.isRequired,
   zoom: PropTypes.number.isRequired,
-  foundItems: PropTypes.arrayOf(Object)
+	foundItems: PropTypes.arrayOf(Object),
+	currentUserLocation: PropTypes.array
 };
